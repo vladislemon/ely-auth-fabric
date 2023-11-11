774910d456e3748b237646155b4573a1ca1dea63
vladislemon <vladislemon@gmail.com>
2023-11-11 15:12:49 +0300

    WIP

---
+++ src/main/java/net/vladislemon/elyauth/mixin/ServerLoginNetworkHandlerMixin.java
@@ -0,0 +1,8 @@
+package net.vladislemon.elyauth.mixin;
+
+import net.minecraft.server.network.ServerLoginNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ServerLoginNetworkHandler.class)
+public class ServerLoginNetworkHandlerMixin {
+}
